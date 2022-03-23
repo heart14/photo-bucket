2c74327b8b232ca99ef044946a0b4cd88da73208
heart14 <lwf14@qq.com>
2022-03-23 16:19:27 +0800

    init project

---
+++ src/main/java/com/heart/photobucket/PhotoBucketApplication.java
@@ -0,0 +1,12 @@
+package com.heart.photobucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoBucketApplication.class, args);
+    }
+}
